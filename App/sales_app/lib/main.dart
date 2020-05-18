95502dd8817378fd10ae64b1893d1858741c1c96
alvarofps <alvarofps@gmail.com>
2020-05-17 23:11:47 -0500

    Create navbar #1

---
+++ App/sales_app/lib/main.dart
@@ -0,0 +1,173 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'models/global.dart';
+
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Daily sales',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+        // This makes the visual density adapt to the platform that you run
+        // the app on. For desktop platforms, the controls will be smaller and
+        // closer together (more dense) than on mobile platforms.
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Simple app to put sales in'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+      return new MaterialApp(
+        home: SafeArea(
+          child: DefaultTabController(
+            length: 3,
+            child: Scaffold(
+              body: Stack(
+                children: <Widget>[
+                  TabBarView(
+                    children:[
+                      new Container(
+                        color: darkGreyColor,
+                      ),
+                      new Container(
+                        color: darkGreyColor,
+                      ),
+                      new Container(
+                        color: darkGreyColor,
+                      ),
+                    ],
+                  ),
+                  Container(
+                    padding: EdgeInsets.only(left: 50),
+                    height: 150,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(45),
+                        bottomRight: Radius.circular(45)
+                      ),
+                      color: Colors.white,
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Text("Intray",style: homePage,),
+                        Container(),
+                      ],
+                    )
+                  ),
+                  Container(
+                    height: 80,
+                    width: 80,
+                    margin: EdgeInsets.only(top: 110, left: 160),
+                    child: FloatingActionButton(
+                      child: Icon(Icons.add, size: 70,),
+                      backgroundColor: redButtonColor,
+                      onPressed: (){},
+                    ),
+                  )
+                ]
+              ),
+                appBar: AppBar(
+                title: new TabBar(
+                    tabs: [
+                      Tab(icon: new Icon(Icons.home),
+                      ),
+                      Tab(icon: new Icon(Icons.call),
+                      ),
+                      Tab(icon: new Icon(Icons.search),
+                      ),
+                    ],
+                  labelColor: darkGreyColor,
+                  unselectedLabelColor: Colors.white,
+                  indicatorSize: TabBarIndicatorSize.label,
+                  indicatorPadding:   EdgeInsets.all(5.0),
+                  indicatorColor: Colors.blueAccent,
+                ),
+                  backgroundColor: Colors.white,
+              ),
+              backgroundColor: Colors.white,
+            ),
+          ),
+        ),
+      );
+  }
+}
+
+class Choice {
+  const Choice({this.title, this.icon});
+
+  final String title;
+  final IconData icon;
+}
+
+const List<Choice> choices = const <Choice>[
+  const Choice(title: 'CAR', icon: Icons.call),
+  const Choice(title: 'BICYCLE', icon: Icons.directions_bike),
+  const Choice(title: 'BOAT', icon: Icons.directions_boat),
+  const Choice(title: 'BUS', icon: Icons.directions_bus),
+  const Choice(title: 'TRAIN', icon: Icons.directions_railway),
+  const Choice(title: 'WALK', icon: Icons.directions_walk),
+];
+
+class ChoiceCard extends StatelessWidget {
+  const ChoiceCard({Key key, this.choice}) : super(key: key);
+
+  final Choice choice;
+
+  @override
+  Widget build(BuildContext context) {
+    final TextStyle textStyle = Theme.of(context).textTheme.headline4;
+    return Card(
+      color: Colors.white,
+      child: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Icon(choice.icon, size: 128.0, color: textStyle.color),
+            Text(choice.title, style: textStyle),
+          ],
+        ),
+      ),
+    );
+  }
+}
